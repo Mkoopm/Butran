b19164f24caa6bf3ffacca04b079bc1ce596a983
Marten Koopmans <36400337+Mkoopm@users.noreply.github.com>
2024-01-24 22:22:26 +0100

    refactor: improve comment and structure

--- src/data_io.py
+++ src/data_io.py
@@ -6,14 +6,13 @@ from typing import Iterator
 from src.core.exceptions import InvalidJsonlFile
 
 
-# add generator for jsonl files
 def read_jsonl_file(file_path: PathLike) -> Iterator[dict]:
+    """Iterator loop over a jsonl file line by line."""
     file_path = Path(file_path)
     with open(file_path, "r") as f:
         for line in f:
             try:
-                json_data = json.loads(line)
-                yield json_data
+                yield json.loads(line)
             except json.JSONDecodeError as e:
                 raise InvalidJsonlFile(
                     f"The file {file_path} does not contain valid jsonl content."
