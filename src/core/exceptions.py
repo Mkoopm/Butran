302c8c106a2470604d5923352e2025ac36680d36
Marten Koopmans <36400337+Mkoopm@users.noreply.github.com>
2024-01-24 23:21:56 +0100

    feat: add a basic CLI

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -2,3 +2,7 @@ class InvalidJsonlFile(Exception):
     """This exception signals that the a given file does not satisfy the jsonl specification."""
 
     pass
+
+
+class InvalidDelimiterError(Exception):
+    """The chosen delimiter is not valid."""
